429b77f0de0dcd86c7a6bf8e2d52c7fe28a405bd
ThanhPham911 <pmthanh89@gmail.com>
2017-04-04 13:17:02 +0700

    Create ActiveUsers.jsx

---
+++ imports/ui/analytics/ActiveUsers.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+//import { createContainer } from 'meteor/react-meteor-data';
+
+import HighChart from '../charts/HighCharts.jsx';
+
+import Papa from 'papaparse';
+import Data from 'highcharts/modules/data.src';
+import Export from 'highcharts/modules/exporting.src';
+import Loading from '../Loading.jsx';
+
+export default class ActiveUsers extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options : {},
+            loading : false
+        }
+
+        this.handleData = this.handleData.bind(this);
+
+    }
+
+    componentDidMount() {
+        this.handleData(this.props);
+        this._isMounted = true;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(this.props.params != nextProps.params){
+            this.handleData(nextProps);
+        }
+    }
+
+    componentWillUnmount(){
+        this._isMounted = false;
+    }
+
+    handleData(props){
+        this.setState({
+            loading : true
+        })
+        var that = this;
+        var csv ;
+        Meteor.call('analytics.ActiveUsers',props.params.dateQuery,props.params.hotspot,props.params.project, function(er, rs){
+            csv = Papa.unparse({
+                fields: ['Categories', 'Visits', 'Unique Visitors'],
+                data: rs.data.aggregations
+            });
+
+            //console.log(csv);
+            if(that._isMounted){
+                that.setState({
+                    options : {
+
+                        data: {
+                            csv: csv
+                        },
+
+                        title: {
+                            text: 'Active Users'
+                        },
+
+                        xAxis: {
+                            tickInterval: 24 * 3600 * 1000, // one week
+                            tickWidth: 0,
+                            
+                        },
+
+                        yAxis: {
+                            title: {
+                                text: 'Daily Visits'
+                            }
+                        },
+
+                        tooltip: {
+                            shared: true,
+                            crosshairs: true
+                        },
+
+                        plotOptions: {
+                            series: {
+                                marker: {
+                                    lineWidth: 1
+                                }
+                            }
+                        },
+
+                        series: [{
+                            name: 'Visits',
+                            lineWidth: 3,
+                            marker: {
+                                radius: 4
+                            }
+                        }, {
+                            name: 'Unique Visitors'
+                        }]
+
+                       
+                    },
+                    loading : false
+                })
+            }
+
+        });
+
+        //}
+    }
+
+    render(){
+        return (
+            <div>
+                {!this.state.loading ? (
+                        <HighChart container='activeUsersChart' options={this.state.options} modules={[Data]} />
+                    ) : <Loading />
+                }
+            </div>
+        );
+    }
+}
