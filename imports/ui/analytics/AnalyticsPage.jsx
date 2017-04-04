399e4765f489cd6e9f170d56129277b8c16b60e1
ThanhPham911 <pmthanh89@gmail.com>
2017-04-04 13:19:00 +0700

    Update AnalyticsPage.jsx

--- imports/ui/analytics/AnalyticsPage.jsx
+++ imports/ui/analytics/AnalyticsPage.jsx
@@ -3,10 +3,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import ActiveUsers from './ActiveUsers.jsx';
-import UniqueUsers from './UniqueUsers.jsx';
 import Visits from './Visits.jsx';
-import InternetAccess from './InternetAccess.jsx';
-import Engagements from './Engagements.jsx';
 import Demographics from './Demographics.jsx';
 
 export default class AnalyticsPage extends Component {
@@ -38,19 +35,7 @@ export default class AnalyticsPage extends Component {
                   <div className="col-md-12">
                       <section className="panel" >
                           <div className="panel-body">
-                              <div className="col-md-3">
-                                  <Link to='/analytics/activity/active' >
-                                      <div className="mini-stat clearfix" >
-                                          <span className="mini-stat-icon tar"><i className="fa fa-user"></i></span>
-                                          <div className="mini-stat-info">
-                                              <span>
-                                                <UniqueUsers params={this.props.params}/>
-                                              </span>
-                                              UNIQUE USERS
-                                          </div>
-                                      </div>
-                                  </Link>
-                              </div>
+                             
                               <div className="col-md-3">
                                   <Link to='/analytics/AppEvents/app_launches' >
                                       <div className="mini-stat clearfix">
@@ -65,31 +50,7 @@ export default class AnalyticsPage extends Component {
                                   </Link>
 
                               </div>
-                              <div className="col-md-3">
-                                  <Link to='/analytics/AppEvents/internet_access' >
-                                      <div className="mini-stat clearfix">
-                                          <span className="mini-stat-icon yellow-b"><i className="fa fa-wifi"></i></span>
-                                          <div className="mini-stat-info">
-                                          <span>
-                                            <InternetAccess params={this.props.params}/>
-                                          </span>
-                                              INTERNET ACCESS
-                                          </div>
-                                      </div>
-                                  </Link>
-
-                              </div>
-                              <div className="col-md-3">
-                                  <div className="mini-stat clearfix">
-                                      <span className="mini-stat-icon orange"><i className="fa fa-hand-pointer-o"></i></span>
-                                      <div className="mini-stat-info">
-                                          <span>
-                                            <Engagements params={this.props.params}/>
-                                          </span>
-                                          ENGAGEMENTS
-                                      </div>
-                                  </div>
-                              </div>
+                             
                              
 
                           </div>
