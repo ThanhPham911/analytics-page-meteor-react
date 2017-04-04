d3a7188a06697774c4f131bbb26cdc774a8b8eb1
ThanhPham911 <pmthanh89@gmail.com>
2017-04-04 12:01:46 +0700

    Update main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -3,8 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import Popup from 'react-popup';
 
-//import Analytics from '../imports/ui/App.jsx';
-
 import { renderRoutes } from '../imports/startup/client/routes.js';
 
 
@@ -12,7 +10,7 @@ Meteor.startup(() => {
     render((
         renderRoutes()
     ), document.getElementById('container'));
-    //render(<Analytics />, document.getElementById('container'));
+    
     render(<Popup
         className="mm-popup"
         btnClass="mm-popup__btn"
@@ -23,5 +21,3 @@ Meteor.startup(() => {
         wildClasses={false}
         />, document.getElementById('popupContainer'));
 });
-
-//render(renderRoutes(), document.getElementById('render-analytics'));
