36ca996b0470cef7341a5f083803574c7369ce6a
ThanhPham911 <pmthanh89@gmail.com>
2017-04-04 13:22:22 +0700

    Create Demographics.jsx

---
+++ imports/ui/analytics/Demographics.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+
+import AgeGroup from './AgeGroup.jsx';
+import Loading from '../Loading.jsx';
+
+export default class Demographics extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Mac : [],
+            loading : false
+        }
+
+        this.handleData = this.handleData.bind(this);
+
+    }
+
+    componentDidMount() {
+        this.handleData(this.props);
+        this._isMounted = true;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(this.props.params != nextProps.params){
+            this.handleData(nextProps);
+        }
+
+    }
+
+    componentWillUnmount(){
+        this._isMounted = false;
+    }
+
+    handleData(props){
+        this.setState({
+            loading : true
+        })
+        var that = this;
+
+        Meteor.call('analytics.getMac',props.params.dateQuery,props.params.hotspot,props.params.project, function(er, rs){
+            if(that._isMounted){
+                that.setState({
+                    Mac : rs,
+                    loading : false
+                })
+            }
+        })
+    }
+
+    render(){
+        return (
+            <div className="row">
+
+                <div className="col-md-3" >
+                    {this.state.loading ? (
+                        <Loading />
+                    ) : ''
+                    }
+                    <AgeGroup Mac={this.state.Mac} display={this.state.loading}/>
+                </div>
+                
+            </div>
+        )
+    }
+}
